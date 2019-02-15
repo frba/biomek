3dbc61675fa64e26442897f084529c4e2e7a7eff
frba <frba@cin.ufpe.br>
2019-02-15 11:34:23 -0500

    changing directory

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Biomek',
+    version='0.2.0',
+    install_requires=['numpy==1.16.1', 'termcolor==1.1.0'])
+    url='',
+    license='',
+    author='frba',
+    author_email='',
+    description=''
+)
